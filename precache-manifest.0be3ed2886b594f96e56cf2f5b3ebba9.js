baf5d819ba0288da847a85ce43804ff0a6abdf02
ram <ramkrishan@kontikilabs.com>
2019-03-15 18:45:12 +0530

    Updates

---
+++ precache-manifest.0be3ed2886b594f96e56cf2f5b3ebba9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b137c71a7fb8bd7b7b4fae73d1002f25",
+    "url": "/rkbhambri/Generate-Google-Fonts/static/media/fallback.b137c71a.gif"
+  },
+  {
+    "revision": "afa26d36e13433c5e5777f23e99a341e",
+    "url": "/rkbhambri/Generate-Google-Fonts/static/media/copy.afa26d36.svg"
+  },
+  {
+    "revision": "5bf1ca27cc8e6398c831",
+    "url": "/rkbhambri/Generate-Google-Fonts/static/js/runtime~main.5bf1ca27.js"
+  },
+  {
+    "revision": "fb952d372f34a3d7f6cd",
+    "url": "/rkbhambri/Generate-Google-Fonts/static/js/main.fb952d37.chunk.js"
+  },
+  {
+    "revision": "1c6129b2a0972ead14e4",
+    "url": "/rkbhambri/Generate-Google-Fonts/static/js/2.1c6129b2.chunk.js"
+  },
+  {
+    "revision": "fb952d372f34a3d7f6cd",
+    "url": "/rkbhambri/Generate-Google-Fonts/static/css/main.4e3dfa7f.chunk.css"
+  },
+  {
+    "revision": "1c6129b2a0972ead14e4",
+    "url": "/rkbhambri/Generate-Google-Fonts/static/css/2.a73232e3.chunk.css"
+  },
+  {
+    "revision": "e8e0cdc0b95dbde09041e3bc378cc822",
+    "url": "/rkbhambri/Generate-Google-Fonts/index.html"
+  }
+];
